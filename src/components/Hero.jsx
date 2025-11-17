6efa67339fe590c2465d3336b51f5e1e20dbb598
Nizar4code <4hmadanbiy4@gmail.com>
2025-11-17 17:53:04 +0700

    update Hero - background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./styles/Hero.css";
+import videoHero from "../assets/Rain-and-Blue.mp4"
 
 export default function Hero() {
   return (
     <section className="hero-section">
       <div className="hero-frame">
-        Hero Section
+        <div className="overlay"></div>
+        <video src={videoHero} autoPlay loop muted disablePictureInPicture>
+        </video>
+        <div className="hero-background-blur">
+            content
+        </div>
       </div>
     </section>
   );
